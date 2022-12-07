c7122c073e5ac9f484be432c4e503735038d4426
Lucas <lucasmagno695@gmail.com>
2022-12-07 15:51:04 -0300

    projeto root

---
+++ cais/src/main/java/br/com/compass/cais/CaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.cais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaisApplication.class, args);
+	}
+
+}
---
+++ cais/src/test/java/br/com/compass/cais/CaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.cais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
