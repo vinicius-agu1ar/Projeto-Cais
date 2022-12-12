ecbd5ee3947acf8d88fcf960274e055c69c5e588
Vinicius Aguiar <vinimoreiraguiar@hotmail.com>
2022-12-12 16:04:57 -0300

    Criação do PierRequestDTO e PierResponseDTO

---
+++ cais/src/main/java/br/com/compass/cais/dto/request/PierRequestDTO.java
@@ -0,0 +1,31 @@
+package br.com.compass.cais.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import br.com.compass.cais.enums.Origin;
+
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PierRequestDTO {
+
+    @NotBlank
+    private String cnpj;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Origin Origin;
+
+
+}
