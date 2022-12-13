dad1007f0afafc075ec8894b54fa2f8f569974f7
Vinicius Aguiar <vinimoreiraguiar@hotmail.com>
2022-12-13 17:27:48 -0300

    Criação dos endpoints Post e Delete e update no ExceptionHandler

--- cais/src/main/java/br/com/compass/cais/controller/CompanyController.java
+++ cais/src/main/java/br/com/compass/cais/controller/CompanyController.java
@@ -2,16 +2,22 @@ package br.com.compass.cais.controller;
 
 import br.com.compass.cais.entites.Company;
 import br.com.compass.cais.repository.CompanyRepository;
+import br.com.compass.cais.services.CompanyService;
 import br.com.compass.cais.services.assembler.CompanyDTOAssembler;
+import br.com.compass.cais.services.assembler.CompanyInputDisassembler;
+import br.com.compass.cais.services.dto.request.CompanyRequestDTO;
 import br.com.compass.cais.services.dto.response.CompanyResponseDTO;
+import br.com.compass.cais.exceptions.CompanyNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -21,8 +27,10 @@ import java.util.List;
 public class CompanyController {
 
     private final CompanyRepository repository;
-
+    private final CompanyService companyService;
     private final CompanyDTOAssembler assembler;
+    private final CompanyInputDisassembler disassembler;
+    private final CompanyRepository companyRepository;
 
     @GetMapping
     public Page<CompanyResponseDTO> listCompanies(Pageable pagination) {
@@ -32,5 +40,22 @@ public class CompanyController {
         return pagesDTO;
     }
 
+    @PostMapping
+    public ResponseEntity<CompanyResponseDTO> create(@RequestBody @Valid CompanyRequestDTO request) {
+        Company company = disassembler.toDomainObject(request);
+        company = companyService.adicionar(company);
+        CompanyResponseDTO response = assembler.toModel(company);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
+    @DeleteMapping ("/id")
+    public void delete (Long id){
+        getCompany(id);
+        companyRepository.deleteById(id);
+    }
+
+    private Company getCompany(Long id) {
+        return companyRepository.findById(id)
+                .orElseThrow(CompanyNotFoundException::new);
+    }
 }
