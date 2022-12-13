0337f3ad6a75c0a3c54226913bc28370f4f9fe29
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-13 13:08:25 -0300

    Criando EndPoint get e o controller

--- cais/src/main/java/br/com/compass/cais/repository/CompanyRepository.java
+++ cais/src/main/java/br/com/compass/cais/repository/CompanyRepository.java
@@ -2,5 +2,6 @@
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository <Company,long>{
+    Page<Company> findByName(String name, Pageable pagination);
 
 }
