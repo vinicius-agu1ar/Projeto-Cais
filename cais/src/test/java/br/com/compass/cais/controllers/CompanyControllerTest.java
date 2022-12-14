af2a43a98f078ec19ec7af38ff4748e4c3ddb24b
Vinicius Aguiar <vinimoreiraguiar@hotmail.com>
2022-12-14 17:23:42 -0300

    Criação das classes de utils e testes do Controller

---
+++ cais/src/test/java/br/com/compass/cais/controllers/CompanyControllerTest.java
@@ -0,0 +1,130 @@
+package br.com.compass.cais.controllers;
+
+import br.com.compass.cais.controller.CompanyController;
+import br.com.compass.cais.enums.Origin;
+import br.com.compass.cais.repository.CompanyRepository;
+import br.com.compass.cais.services.CompanyService;
+import br.com.compass.cais.services.assembler.CompanyDTOAssembler;
+import br.com.compass.cais.services.assembler.CompanyInputDisassembler;
+import br.com.compass.cais.services.dto.request.CompanyRequestDTO;
+import br.com.compass.cais.services.dto.response.CompanyResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import br.com.compass.cais.utils.TestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+@WebMvcTest(controllers = CompanyController.class)
+@AutoConfigureMockMvc(addFilters = false) // ignorando a camada do security
+class CompanyControllerTest {
+
+    public static final String BASE_URL = "/api/company";
+    public static final String ID_URL = BASE_URL + "/1";
+    public static final Long ID = 1L;
+    @MockBean
+    private CompanyRepository repository;
+    @MockBean
+    private CompanyService service;
+    @MockBean
+    private CompanyDTOAssembler assembler;
+    @MockBean
+    private CompanyInputDisassembler disassembler;
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    void findAll() throws Exception {
+        List<CompanyResponseDTO> companies = Arrays.asList(new CompanyResponseDTO());
+        Page<CompanyResponseDTO> page = new PageImpl<>(companies);
+        when(service.findAll(any(Pageable.class))).thenReturn(page);
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.get(BASE_URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        MockHttpServletResponse resposta = result.getResponse();
+        assertEquals(HttpStatus.OK.value(), resposta.getStatus());
+    }
+
+    @Test
+    void create() throws Exception {
+        CompanyRequestDTO request = getCompanyRequestDTO();
+        CompanyResponseDTO companyResponseDTO = new CompanyResponseDTO();
+        String input = TestUtils.mapToJson(request);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.post(BASE_URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(input)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+    }
+
+    @Test
+    void findById() throws Exception {
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.get(ID_URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+    @Test
+    void update() throws Exception {
+        CompanyRequestDTO request = getCompanyRequestDTO();
+        CompanyResponseDTO companyResponseDTO = new CompanyResponseDTO();
+        String input = TestUtils.mapToJson(request);
+
+        MvcResult result = mvc
+                .perform(MockMvcRequestBuilders.put(ID_URL)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(input)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+//    @Test
+//    void delete() throws Exception {
+//        MvcResult result = mvc
+//                .perform(MockMvcRequestBuilders.delete(ID_URL)
+//                        .accept(MediaType.APPLICATION_JSON)
+//                        .contentType(MediaType.APPLICATION_JSON))
+//                .andReturn();
+//
+//        MockHttpServletResponse response = result.getResponse();
+//
+//        assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+//    }
+
+    private CompanyRequestDTO getCompanyRequestDTO() {
+        return CompanyRequestDTO.builder()
+                .cnpj("Test")
+                .name("Test")
+                .Origin(Origin.INTERNATIONAL)
+                .build();
+    }
+}
