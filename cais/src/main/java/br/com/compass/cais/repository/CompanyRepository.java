0ced923281fb327572a67c97b1e1c99186f05f29
Lucas <lucasmagno695@gmail.com>
2022-12-15 16:31:19 -0300

    Criando funcionalidade de pesquisar empresa por origin e criando classe de serviço para converter a entrada em um Origin, implementando classe WevConfig:  habilitando cors do spring e adicionando conversor para o Enum Origin (facilitando a filtragem de company por Origin)

--- cais/src/main/java/br/com/compass/cais/repository/CompanyRepository.java
+++ cais/src/main/java/br/com/compass/cais/repository/CompanyRepository.java
@@ -1,11 +1,12 @@
 package br.com.compass.cais.repository;
 
 import br.com.compass.cais.entites.Company;
+import br.com.compass.cais.enums.Origin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository <Company, Long>{
-    Page<Company> findByName(String name, Pageable pagination);
+    Page<Company> findByOrigin(Origin origin, Pageable pagination);
 
 }
