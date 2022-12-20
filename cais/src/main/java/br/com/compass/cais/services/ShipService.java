f2ccdd31d9f04b20b3d2927888da69c80015c534
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-20 09:09:33 -0300

    Criação do endPoint Get para listar todos

--- cais/src/main/java/br/com/compass/cais/services/ShipService.java
+++ cais/src/main/java/br/com/compass/cais/services/ShipService.java
@@ -1,18 +1,24 @@
 package br.com.compass.cais.services;
 
+import br.com.compass.cais.entites.Ship;
 import br.com.compass.cais.exceptions.EntityInUseException;
-import br.com.compass.cais.exceptions.PierNotFoundException;
 import br.com.compass.cais.exceptions.ShipNotFoundException;
 import br.com.compass.cais.repository.ShipRepository;
 import br.com.compass.cais.services.assembler.ShipDTOAssembler;
 import br.com.compass.cais.services.assembler.ShipInputDisassembler;
+import br.com.compass.cais.services.dto.response.ShipResponseDTO;
+import org.springframework.data.domain.Slice;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -38,4 +44,12 @@ public class ShipService {
             throw new EntityInUseException();
         }
     }
+
+    public Page<ShipResponseDTO> findAll(Pageable pageable) {
+
+        Page<Ship> pageShip = repository.findAll(pageable);
+        List<ShipResponseDTO> shipResponseDTOS = assembler.toCollectionModel(pageShip.getContent());
+        return new PageImpl<>(shipResponseDTOS, pageable, pageShip.getTotalElements());
+    }
+
 }
