66761afd97fff67ed6e2164f98ad85649afe41db
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-20 08:26:43 -0300

    Construindo o disassembler

--- cais/src/main/java/br/com/compass/cais/services/assembler/ShipInputDisassembler.java
+++ cais/src/main/java/br/com/compass/cais/services/assembler/ShipInputDisassembler.java
@@ -1,9 +1,20 @@
 package br.com.compass.cais.services.assembler;
 
+
+import br.com.compass.cais.entites.Ship;
+import br.com.compass.cais.services.dto.request.ShipRequestDTO;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ShipInputDisassembler {
+
+    private final ModelMapper modelMapper;
+
+    public Ship toDomainObject(ShipRequestDTO shipRequestDTO) {
+
+        return modelMapper.map(shipRequestDTO, Ship.class);
+    }
 }
