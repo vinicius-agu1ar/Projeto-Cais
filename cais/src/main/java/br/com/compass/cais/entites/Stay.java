3725257a242fae40f8c502aef5d49e150cfc0b71
Lucas <lucasmagno695@gmail.com>
2022-12-26 18:48:10 -0300

    criando entidade e tabela stay e inserindo dados

---
+++ cais/src/main/java/br/com/compass/cais/entites/Stay.java
@@ -0,0 +1,33 @@
+package br.com.compass.cais.entites;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EqualsAndHashCode
+@Table(name = "STAY")
+public class Stay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "SHIP_ID")
+    private Ship ship;
+
+    @Column(name = "ENTRY")
+    private LocalDateTime entry;
+
+    @Column(name = "EXIT")
+    private LocalDateTime exit;
+
+    @Column(name = "VALUE")
+    private BigDecimal value;
+}
