811177beefd7d5e6934599183bb592fb15a9daa0
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-27 17:44:19 -0300

    Construção da vinculação(bind) do StayService e StayController

--- cais/src/main/java/br/com/compass/cais/controller/StayController.java
+++ cais/src/main/java/br/com/compass/cais/controller/StayController.java
@@ -1,13 +1,25 @@
 package br.com.compass.cais.controller;
 
+import br.com.compass.cais.services.StayService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/stay")
 public class StayController {
+
+    private final StayService stayService;
+    @PostMapping("/{id}/stay/{stayId}")
+    public ResponseEntity<Void> bindStayShip(@PathVariable("id") Long id, @PathVariable("shipId") Long shipId) {
+        log.info("Vinculando um Stay a um Ship...");
+        stayService.bind(id,shipId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
--- cais/src/main/java/br/com/compass/cais/services/StayService.java
+++ cais/src/main/java/br/com/compass/cais/services/StayService.java
@@ -1,11 +1,33 @@
 package br.com.compass.cais.services;
 
+import br.com.compass.cais.entites.Ship;
+import br.com.compass.cais.entites.Stay;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+import static org.hibernate.criterion.Example.create;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class StayService {
+
+    private final ShipService shipService;
+
+    @Transactional
+    public void bind(Long id, Long shipId){
+        log.info("Chamando método bind - Service Stay");
+        Ship ship = shipService.fetchOrFail(shipId);
+        Stay stay = new Stay();
+        stay.setShip(ship);
+        stay.setEntry(LocalDateTime.now());
+
+        create(stay);
+
+    }
+
 }
