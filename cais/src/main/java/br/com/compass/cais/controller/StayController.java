d91c5644f6efd165e5123beec07d43f81a07211d
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-27 18:33:42 -0300

    StayController bing

--- cais/src/main/java/br/com/compass/cais/controller/StayController.java
+++ cais/src/main/java/br/com/compass/cais/controller/StayController.java
@@ -1,13 +1,20 @@
 package br.com.compass.cais.controller;
 
 import br.com.compass.cais.services.StayService;
+import br.com.compass.cais.services.dto.request.PierRequestDTO;
+import br.com.compass.cais.services.dto.request.StayRequestDTO;
+import br.com.compass.cais.services.dto.response.pier.PierResponseDTO;
+import br.com.compass.cais.services.dto.response.stay.StayResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,11 +22,14 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api/stay")
 public class StayController {
 
-    private final StayService stayService;
+    private final StayService service;
+
+
+
     @PostMapping("/{id}/stay/{stayId}")
     public ResponseEntity<Void> bindStayShip(@PathVariable("id") Long id, @PathVariable("shipId") Long shipId) {
         log.info("Vinculando um Stay a um Ship...");
-        stayService.bind(id,shipId);
+        service.bind(id,shipId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- cais/src/main/java/br/com/compass/cais/services/StayService.java
+++ cais/src/main/java/br/com/compass/cais/services/StayService.java
@@ -2,6 +2,8 @@ package br.com.compass.cais.services;
 
 import br.com.compass.cais.entites.Ship;
 import br.com.compass.cais.entites.Stay;
+import br.com.compass.cais.services.dto.request.StayRequestDTO;
+import br.com.compass.cais.services.dto.response.stay.StayResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
