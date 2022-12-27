4e91d53671467ddeeec5a1cfd3c7b90579769c49
Lucas <lucasmagno695@gmail.com>
2022-12-27 10:07:14 -0300

    criando a classe service e controller para a entidade Stay

---
+++ cais/src/main/java/br/com/compass/cais/controller/StayController.java
@@ -0,0 +1,13 @@
+package br.com.compass.cais.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stay")
+public class StayController {
+}
