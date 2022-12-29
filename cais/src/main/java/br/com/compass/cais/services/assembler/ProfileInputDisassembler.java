70d254c175532ed8a9cf6643c03c7a8c01005990
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-29 09:56:57 -0300

    Criação dos DTOs, dos Assembler e Disassembler, endpoint, service e repository

---
+++ cais/src/main/java/br/com/compass/cais/services/assembler/ProfileInputDisassembler.java
@@ -0,0 +1,18 @@
+package br.com.compass.cais.services.assembler;
+
+import br.com.compass.cais.entites.Profile;
+import br.com.compass.cais.services.dto.request.ProfileRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProfileInputDisassembler {
+
+    private final ModelMapper modelMapper;
+
+    public Profile toDomainObject(ProfileRequestDTO profileRequestDTO) {
+        return modelMapper.map(profileRequestDTO, Profile.class);
+    }
+}
---
+++ cais/src/main/java/br/com/compass/cais/services/dto/response/profile/ProfileResponseDTO.java
@@ -0,0 +1,13 @@
+package br.com.compass.cais.services.dto.response.profile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileResponseDTO {
+
+    private Long id;
+
+    private String name;
+}
