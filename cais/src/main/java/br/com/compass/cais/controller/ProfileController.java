8c62f238b2d31531dd0924116784f30311bc2659
Nalbert Bastos <nalbertbastos@outlook.com>
2022-12-29 15:02:50 -0300

    Criando endPoint delete

--- cais/src/main/java/br/com/compass/cais/controller/ProfileController.java
+++ cais/src/main/java/br/com/compass/cais/controller/ProfileController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,5 +24,12 @@ public class ProfileController {
         log.info("Criando um novo Profile...");
         ProfileResponseDTO response = service.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (@PathVariable("id") Long id){
+        log.info("Excluindo uma Profile por Id...");
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
